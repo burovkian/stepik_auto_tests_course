62f8350d477bb615c788dafe0c37ae8277b8282b
Kirill Burov <burov.kian@gmail.com>
2025-02-02 03:08:16 +0400

    добавление шага 8

---
+++ stepik_at/step3/lesson2_step8.py
@@ -0,0 +1,7 @@
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+
+a = 11
+b = 15
+
+test_input_text(a,b)
